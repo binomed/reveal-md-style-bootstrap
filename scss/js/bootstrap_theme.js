4bc751917943f9c3c7cc07a938f629c976316010
JefBinomed <jean.francois.garreau@gmail.com>
2014-03-04 15:23:33 +0100

    add hilihgt managment

--- scss/js/bootstrap_theme.js
+++ scss/js/bootstrap_theme.js
@@ -3,9 +3,25 @@ var BOOTSTRAP_THEME = BOOTSTRAP_THEME || function(){
 
 	function pageLoad(){
 		
+		postProdCodeHilight();
 	
 		if (Reveal){
 			Reveal.sync();
+		}
+	}
+
+	function postProdCodeHilight(){
+		var array = document.querySelectorAll('code.toHilight');
+		for (var i =0; i <array.length; i++){
+			var length = 0;
+			var textCode = array[i].innerHTML;
+			do{
+				length = textCode.length;
+				textCode = textCode.replace('&lt;mark&gt;', '<mark>');
+				textCode = textCode.replace('&lt;/mark&gt;', '</mark>');
+			}while(length != textCode.length);
+			array[i].innerHTML = textCode;
+
 		}
 	}
 
